8d852821fa70e1815891fa09cbfd150c53ca0bb3
Zhaopeng Zhang <zhangzhaopeng@mail.nankai.edu.cn>
2018-03-22 20:33:43 +0800

    Move to python3.

--- pronun.py
+++ pronun.py
@@ -3,7 +3,7 @@
 get us pronunciation from
 https://dictionary.cambridge.org/pronunciation/english
 """
-from urllib import urlopen
+from urllib.request import urlopen
 import sys
 from playsound import playsound
 import re
@@ -14,7 +14,7 @@ def pro(word):
     url = "https://dictionary.cambridge.org/pronunciation/english/{}".\
           format(word)
     content = urlopen(url).read()
-    urls = set(re.findall('data-src-mp3="(.*?(uk|us)_pron.*?.mp3)', content))
+    urls = set(re.findall('data-src-mp3="(.*?(uk|us)_pron.*?.mp3)', str(content)))
     for u, s in urls:
         # print("Now playing: pronunciation of {}".format(s))
         if s == 'us':
@@ -26,6 +26,6 @@ if __name__ == '__main__':
         word = sys.argv[1]
     except Exception as e:
         print("Usage: python pronun.py hello")
-        print("Error: {}".format(e))
+        print(("Error: {}".format(e)))
         sys.exit(1)
     pro(word)
